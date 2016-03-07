53d5dd1e683a3e92395f6cf92f894571fbdfeb08
JunYuan <junyuancs@gmail.com>
2016-03-06 21:34:49 -0800

    Transplant project to Spark platform
    Remember that this program read data from my txt files which are the
    output of ToSpark.py program. I use naive bayes algorithm from MLlib, a
    ML library based on Spark.
    In this program, I can process all 1247391 tweets stored in my database.
    If you want to process much more data, you can try to process data on
    spark all the time, instead of read data from txt file.

---
+++ PySpark.py
@@ -0,0 +1,26 @@
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.regression import LabeledPoint
+
+def parseLine(line):
+    parts = line.split(', #')
+    label = float(parts[0])
+    features = Vectors.dense([float(x) for x in parts[1].split('#')])
+    return LabeledPoint(label, features)
+
+tr1 = sc.textFile('/Users/yuanjun/Desktop/train1.txt').map(parseLine)
+tr2 = sc.textFile('/Users/yuanjun/Desktop/train2.txt').map(parseLine)
+tr3 = sc.textFile('/Users/yuanjun/Desktop/train3.txt').map(parseLine)
+tr4 = sc.textFile('/Users/yuanjun/Desktop/train4.txt').map(parseLine)
+te1 = sc.textFile('/Users/yuanjun/Desktop/test1.txt').map(parseLine)
+te2 = sc.textFile('/Users/yuanjun/Desktop/test2.txt').map(parseLine)
+
+tr1 = tr1.union(tr2)
+tr3 = tr3.union(tr4)
+train = tr1.union(tr3)
+test = te1.union(te2)
+
+model = NaiveBayes.train(train, 1.0)
+predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
+accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
+print accuracy
